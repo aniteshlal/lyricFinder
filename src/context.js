0cf00635312a171de29cc00d6d74438423d5cd6f
Anitesh Lal <alal@alamedaelectric.com>
2019-07-19 10:36:01 -0700

    Created Lyric Compenet and used api to get lyric data

--- src/context.js
+++ src/context.js
@@ -4,13 +4,13 @@ const Context = React.createContext();
 export class Provider extends Component {
     state = {
         track_list: [],
-        heading: 'Top 10 Tacks'
+        heading: 'Top 10 Tracks'
     }
 
     componentDidMount() {
         axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`)
             .then(res => {
-                console.log(res.data)
+                // console.log(res.data)
                 this.setState({ track_list: res.data.message.body.track_list })
             })
             .catch(err => console.log(err));
